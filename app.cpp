ad5248d18bca4c0f94978d4bd03bd958c63ab259
habibullah6 <nomanhabibullah65@gmail.com>
2025-03-04 19:05:32 +0600

    done

--- app.cpp
+++ app.cpp
@@ -136,30 +136,60 @@ using namespace std;
 
 // Pattern - 4: Right-Angled Number Pyramid - II
 
-void pattern4(int N)
-{
+// void pattern4(int N)
+// {
    
-    for (int i = 1; i <= N; i++)
-    {
+//     for (int i = 1; i <= N; i++)
+//     {
         
-        for (int j = 1; j <=i; j++)
-        {
-           cout <<i<<" ";
-        }
+//         for (int j = 1; j <=i; j++)
+//         {
+//            cout <<i<<" ";
+//         }
        
     
+//         cout << endl;
+//     }
+// }
+
+// Pattern-5: Inverted Right Pyramid
+
+
+
+void pattern5(int N)
+{
+    // This is the outer loop which will loop for the rows.
+    for (int i = 0; i < N; i++)
+    {
+        // This is the inner loop which loops for the columns
+       // no. of columns = (N - row index) for each line here.
+        for (int j =N; j>i; j--)
+        {
+            cout <<"* ";
+        }
+       
+        // As soon as stars for each iteration are printed, we move to the
+        // next row and give a line break otherwise all stars
+        // would get printed in 1 line.
         cout << endl;
     }
 }
 
 
-int main()
+int main(int t)
 {   
     // Here, we have taken the value of N as 5.
     // We can also take input from the user.
-    int N;
-    cin >> N;
-    pattern4(N);
+
+    cin >> t;
+   
+    for (int i = 0; i < t; i++)
+    {
+        int N;
+        cin >> N;
+        pattern5(N);
+    };
+    
 
     return 0;
 }
